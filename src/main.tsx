094455de7e11bb6364d7549732871d870cc0127f
amine <boukhrisamine210@gmail.com>
2024-03-17 11:08:44 +0100

    removed unused declaration

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-        <ChakraProvider theme={theme}>
-            <App />
-        </ChakraProvider>
+    <ChakraProvider theme={theme}>
+        <App />
+    </ChakraProvider>
 );
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -23,39 +23,3 @@ export const fetchCityData = async (city: string) => {
         throw new Error(error)
     }
 }
-
-// const data = {
-    //     coord: { lon: -80.1937, lat: 25.7743 },
-    //     weather: [
-    //         {
-    //             id: 803,
-    //             main: "Clouds",
-    //             description: "broken clouds",
-    //             icon: "04d",
-    //         },
-    //     ],
-    //     base: "stations",
-    //     main: {
-    //         temp: 29.05,
-    //         feels_like: 32.65,
-    //         temp_min: 27.79,
-    //         temp_max: 30.57,
-    //         pressure: 1016,
-    //         humidity: 69,
-    //     },
-    //     visibility: 10000,
-    //     wind: { speed: 4.12, deg: 190 },
-    //     clouds: { all: 75 },
-    //     dt: 1710606475,
-    //     sys: {
-    //         type: 2,
-    //         id: 2009435,
-    //         country: "US",
-    //         sunrise: 1710588508,
-    //         sunset: 1710631807,
-    //     },
-    //     timezone: -14400,
-    //     id: 4164138,
-    //     name: "Miami",
-    //     cod: 200,
-    // };
